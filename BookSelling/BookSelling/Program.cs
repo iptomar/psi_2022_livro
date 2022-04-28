9e1dad17b820bd568f865b550a6f26b85d8b73d7
Antonio Fialho <80701611+t0t0cas@users.noreply.github.com>
2022-04-28 16:02:50 +0100

    Creation of Login and Register Part

--- BookSelling/BookSelling/Program.cs
+++ BookSelling/BookSelling/Program.cs
@@ -3,6 +3,13 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection");;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();;
 
 
 // Add services to the container.
