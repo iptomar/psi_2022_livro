eda6606f2b021260dfe76edccd0acf98430b3cfb
JMatos38 <80701898+JMatos38@users.noreply.github.com>
2022-06-02 11:36:00 +0100

    tentativa de fix, possivel necessidade de rollback

---
+++ BookSelling/BookSelling/Data/Migrations/20220505153426_teste12345.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookSelling.Data.Migrations
+{
+    public partial class teste12345 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "IdCategory",
+                keyValue: 9,
+                column: "NameCategory",
+                value: "Erotic");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "IdCategory",
+                keyValue: 9,
+                column: "NameCategory",
+                value: "Manga");
+        }
+    }
+}
