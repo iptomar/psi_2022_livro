537e4ae67834ac82bc1e6203826862fbbd646125
Antonio Fialho <80701611+t0t0cas@users.noreply.github.com>
2022-06-02 20:19:52 +0100

    Criação do Controller e da View dos Favoritos
    Falta apenas o css para deixar mais bonito

---
+++ BookSelling/BookSelling/Controllers/FavoritesController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookSelling.Data;
+using BookSelling.Models;
+
+namespace BookSelling.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FavoritesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Favorites
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Favorites.Include(f => f.Advertisement).Include(f => f.Utilizadores);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Favorites/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Favorites == null)
+            {
+                return NotFound();
+            }
+
+            var favorite = await _context.Favorites
+                .Include(f => f.Advertisement)
+                .Include(f => f.Utilizadores)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            return View(favorite);
+        }
+
+        // GET: Favorites/Create
+        public IActionResult Create()
+        {
+            ViewData["AdvertisementID"] = new SelectList(_context.Advertisement, "AdID", "ISBM");
+            ViewData["UtilizadoresID"] = new SelectList(_context.Utilizadores, "UserID", "Area");
+            return View();
+        }
+
+        // POST: Favorites/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AdvertisementID,UtilizadoresID")] Favorite favorite)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(favorite);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AdvertisementID"] = new SelectList(_context.Advertisement, "AdID", "ISBM", favorite.AdvertisementID);
+            ViewData["UtilizadoresID"] = new SelectList(_context.Utilizadores, "UserID", "Area", favorite.UtilizadoresID);
+            return View(favorite);
+        }
+
+        // GET: Favorites/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Favorites == null)
+            {
+                return NotFound();
+            }
+
+            var favorite = await _context.Favorites.FindAsync(id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+            ViewData["AdvertisementID"] = new SelectList(_context.Advertisement, "AdID", "ISBM", favorite.AdvertisementID);
+            ViewData["UtilizadoresID"] = new SelectList(_context.Utilizadores, "UserID", "Area", favorite.UtilizadoresID);
+            return View(favorite);
+        }
+
+        // POST: Favorites/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AdvertisementID,UtilizadoresID")] Favorite favorite)
+        {
+            if (id != favorite.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(favorite);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FavoriteExists(favorite.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AdvertisementID"] = new SelectList(_context.Advertisement, "AdID", "ISBM", favorite.AdvertisementID);
+            ViewData["UtilizadoresID"] = new SelectList(_context.Utilizadores, "UserID", "Area", favorite.UtilizadoresID);
+            return View(favorite);
+        }
+
+        // GET: Favorites/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Favorites == null)
+            {
+                return NotFound();
+            }
+
+            var favorite = await _context.Favorites
+                .Include(f => f.Advertisement)
+                .Include(f => f.Utilizadores)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            return View(favorite);
+        }
+
+        // POST: Favorites/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Favorites == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Favorites'  is null.");
+            }
+            var favorite = await _context.Favorites.FindAsync(id);
+            if (favorite != null)
+            {
+                _context.Favorites.Remove(favorite);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FavoriteExists(int id)
+        {
+          return _context.Favorites.Any(e => e.Id == id);
+        }
+    }
+}
