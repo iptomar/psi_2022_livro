154a93d1bf1299da45473c5834860b6068568b85
Antonio Fialho <80701611+t0t0cas@users.noreply.github.com>
2022-07-09 02:03:08 +0100

    Teste

---
+++ BookSelling/QRCode1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using IronBarCode;
+
+namespace QRCode_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneratedBarcode barcode = IronBarCode.BarcodeWriter.CreateBarcode("https://www.youtube.com/watch?v=d-tx9D4a8dc&ab_channel=JovemDionisio", BarcodeEncoding.QRCode);
+            barcode.AddAnnotationTextAboveBarcode("Abre");
+            barcode.SaveAsPng("barcode.png");
+        }
+    }
+}
